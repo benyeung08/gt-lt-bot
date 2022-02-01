d2cf73550213999cd8b8d35e0c10ca06bfbd0c0f
liyoujun <58936872+tooty-1135@users.noreply.github.com>
2022-02-01 03:45:04 +0000

    update

--- cogs/event.py
+++ cogs/event.py
@@ -11,14 +11,17 @@ class Event(commands.Cog):
     #排除自己的訊息，避免陷入無限循環
     if message.author == self.bot.user:
       return
-      
-    if message.content == 'hi' or message.content == 'hello':
-      tmp=["hi","hihi","嗨","嗨嗨","hello","hello there"]
-      await message.channel.send(random.choice(tmp))
+    try:
+      if message.content == 'hi' or message.content == 'hello':
+        tmp=["hi","hihi","嗨","嗨嗨","hello","hello there","哈囉"]
+        await message.channel.send(random.choice(tmp))
 
-    if message.content == '安安':
-      tmp=["安安阿","安安","你好","你好!"]
-      await message.channel.send(random.choice(tmp))
+      if message.content == '安安':
+        tmp=["安安阿","安安","你好","你好!","嗨"]
+        await message.channel.send(random.choice(tmp))
+    except:
+      return
+
   @commands.Cog.listener(name='on_command')
   async def print(self, ctx):
     server = ctx.guild.name
